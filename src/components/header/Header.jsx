4ba8cc725d9324efb28a1dae8b87f858482d25c6
trung3110 <tn0009592@gmail.com>
2021-12-10 16:51:55 +0700

    footer and header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import '../../assets/styles/Header.scss'
 
-const Header = () => {
+function Header() {
     return (
-        <div className="header">
-            header
+        <div className="Header">
+            <div className="Header-container">
+                <span className="header_left">
+                    <div className="logo"></div>
+                    <span>Tổng cục dân số Việt Nam</span>
+                </span>
+                <span className="header_right">
+                    <button className='btn'>Trang chủ</button>
+                    <button className="btn">Thông tin</button>
+                    <button className="btn">Tra cứu</button>
+                    <button className="btn">Đăng nhập</button>
+                </span>
+            </div>
         </div>
     )
 }
