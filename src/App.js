833e1befe1973a1c5432f8bce3b257327f5586e0
trung3110 <tn0009592@gmail.com>
2021-12-16 22:20:34 +0700

    footer

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
             <Route path={`population`} element={<ViewPopulation />} />
           </Route>
         </Routes>
-        {/* <div class="Footer"><Footer/></div> */}
+        <div class="Footer"><Footer/></div>
       </div>
     </BrowserRouter>
   );
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import '../../assets/styles/Footer.scss'
+import {PhoneFilled,MailFilled,HomeFilled } from '@ant-design/icons'
+
 
 export default function Footer() {
     return (
         <div className='Container'>
             <div className='Footer'>
-                <span className='info'>
-                    <p>Địa chỉ : Tầng 4 , tòa nhà Keangnam </p>
-                    <p>Email:Tongcucdanso@gov.vn</p>
-                    <p>Tổng đài hỗ trợ :190022222</p>
-                    <p>Số fax:07237478263</p>
-                </span>
+                <div className='info'>
+                    <h5>Cổng thông tin điện tử Tổng cục dân số - Kế hoạch hóa gia đình ( Bộ Y tế )</h5>
+                    <p>Chịu trách nhiệm chính: Ông  Nguyễn Văn A - Giám đốc Trung tâm thông tin - Bộ Y tế.</p>
+                    <p>Bản quyền thuộc Bộ Y tế.<span>&#174;</span></p>
+                    <p>Ghi rõ nguồn  "Cổng thông tin điện tử Tổng cục dân số" khi phát hành thông tin từ website này.</p>
+                </div>
+                <div className="contact">
+                    <h6>Liên hệ :</h6>
+                    <p><PhoneFilled/><span>0234234234</span></p>
+                    <p><MailFilled /> <span>tcdsvn@gov.vn</span></p>
+                    <p><HomeFilled /><span>Tòa nhà lô D20 - Ngõ số 8 Tôn Thất Thuyết – Mỹ Đình 2 – Nam Từ Liêm - Hà Nội</span></p>
+
+                </div>
             </div>
         </div>
     )
